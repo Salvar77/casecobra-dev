754545346b244d7eb8f7168444c56121d741f239
Łukasz <lukaszkus77@gmail.com>
2025-01-21 21:50:28 +0100

    first commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,12 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+
+export const GET = async (req, res) => {
+  try {
+    console.log("Headers:", req.headers);
+    console.log("Cookies:", req.cookies);
+    await handleAuth()(req, res);
+  } catch (error) {
+    console.error("handleAuth error:", error);
+    res.status(500).json({ error: "Kinde callback error" });
+  }
+};
